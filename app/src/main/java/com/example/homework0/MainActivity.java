b1a42a040be47ec24f2b9d895482edc916076feb
TsaniRogers <tsrogers@bu.edu>
2023-10-09 19:48:24 -0400

    Compiling code

---
+++ app/src/main/java/com/example/homework0/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.homework0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//import com.google.android.gms.location.FusedLocationProviderClient;
+
+
+public class MainActivity extends AppCompatActivity {
+    //private FusedLocationProviderClient locationClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
